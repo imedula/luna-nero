46cc0ca2d739bddcc0b17c3a664c41164ce6781a
iu.medulashvili@gmail.com <iu.medulashvili@gmail.com>
2025-02-09 23:44:01 +0400

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,38 @@
+
+import logo from './logo.png'
+import angelcard from './angelcard.jpg'
+import blackgrimuar from './blackgrimuar.png'
+import fortuna from './fortuna.png'
+import gipsywitch from './gipsywitch.png'
+import harmonictaro from './harmonictaro.png'
+import labradoriti from './labradoriti.png'
+import occulttarot from './occulttarot.jpg'
+import runesoracle from './runesoracle.png'
+import santamuerte from './santamuerte.jpg'
+import santeli from './santeli.jpg'
+import tilismagolden from './tilismagolden.png'
+import tilismasilver from './tilismasilver.png'
+import logoluna from './logoluna.png'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+
+
+
+export const assets = {
+    logoluna,
+    logo,
+    angelcard,
+    blackgrimuar,
+    fortuna,
+    gipsywitch,
+    harmonictaro,
+    labradoriti,
+    occulttarot,
+    runesoracle,
+    santamuerte,
+    santeli,
+    tilismagolden,
+    tilismasilver,
+    search_icon,
+    basket_icon,
+}
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+
+  const [menu,setMenu] = useState("menu");
+
+
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo'  />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>მთავარი</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>პროდუქტი</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>აპლიკაცია</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>ჩვენს შესახებ</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-serach-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>შესვლა</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
