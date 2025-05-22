5cf263322f099767007a17357f0e590f5cd231e1
maxxxara <dati.maxarashvili24@gmail.com>
2025-05-22 16:11:59 +0400

    refactor asset imports for consistency and readability

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,222 +1,217 @@
-
 // menu list
-import candle_1 from './candle_1.jpg'
-import literature_1 from './literature_1.jpg'
-import statue_2 from './statue_2.jpg'
-import crystal_1 from './crystal_1.jpg'
-import oil_1 from './oil_1.jpg'
-import plant_1 from './plant_1.jpg'
-import talisman_1 from './talisman_1.jpg'
-import tarot_1 from './tarot_1.jpg'
-import esoteric_item from './esoteric_item.jpg'
-import accessories_1 from './accessories_1.jpg'
-import fengshui_1 from './fengshui_1.jpg'
-import sticks_1 from './sticks_1.jpg'
+import candle_1 from "./candle_1.jpg";
+import literature_1 from "./literature_1.jpg";
+import statue_2 from "./statue_2.jpg";
+import crystal_1 from "./crystal_1.jpg";
+import oil_1 from "./oil_1.jpg";
+import plant_1 from "./plant_1.jpg";
+import talisman_1 from "./talisman_1.jpg";
+import tarot_1 from "./tarot_1.jpg";
+import esoteric_item from "./esoteric_item.jpg";
+import accessories_1 from "./accessories_1.jpg";
+import fengshui_1 from "./fengshui_1.jpg";
+import sticks_1 from "./sticks_1.jpg";
 
 // items for sale
-import item_1 from './item_1.png'
-import item_2 from './item_2.png'
-import item_3 from './item_3.png'
-import item_4 from './item_4.png'
-import item_5 from './item_5.jpg'
-import item_6 from './item_6.png'
-import item_7 from './item_7.jpg'
-import item_8 from './item_8.png'
-import item_9 from './item_9.jpg'
-import item_10 from './item_10.png'
-import item_11 from './item_11.png'
-import item_12 from './item_12.jpg'
-
+import item_1 from "./item_1.png";
+import item_2 from "./item_2.png";
+import item_3 from "./item_3.png";
+import item_4 from "./item_4.png";
+import item_5 from "./item_5.jpg";
+import item_6 from "./item_6.png";
+import item_7 from "./item_7.jpg";
+import item_8 from "./item_8.png";
+import item_9 from "./item_9.jpg";
+import item_10 from "./item_10.png";
+import item_11 from "./item_11.png";
+import item_12 from "./item_12.jpg";
 
 // page assets
-import header_room from './header_room.jpg'
-import logo from './logo.png'
-import basket_icon from './basket_icon.png'
-import search_icon from './search_icon.png'
-import add_icon_green from './add_icon_green.png'
-import add_icon_white from './add_icon_white.png'
-import bag_icon from './bag_icon.png'
-import cross_icon from './cross_icon.png'
-import logout_icon from './logout_icon.png'
-import parcel_icno from './parcel_icon.png'
-import  rating_starts from './rating_starts.png'
-import remove_icon_red from './remove_icon_red.png'
-import selector_icon from './selector_icon.png'
-import tiktok_logo from'./tiktok_logo.png'
-import instagram_logo from './instagram_logo.png'
-import facebook_logo from './facebook_logo.png'
-import profile_icon from './profile_icon.png'
-
-
+import header_room from "./header_room.jpg";
+import logo from "./logo.png";
+import basket_icon from "./basket_icon.png";
+import search_icon from "./search_icon.png";
+import add_icon_green from "./add_icon_green.png";
+import add_icon_white from "./add_icon_white.png";
+import bag_icon from "./bag_icon.png";
+import cross_icon from "./cross_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icno from "./parcel_icon.png";
+import rating_starts from "./rating_starts.png";
+import remove_icon_red from "./remove_icon_red.png";
+import selector_icon from "./selector_icon.png";
+import tiktok_logo from "./tiktok_logo.png";
+import instagram_logo from "./instagram_logo.png";
+import facebook_logo from "./facebook_logo.png";
+import profile_icon from "./facebook_logo.png";
 
 export const assets = {
-    basket_icon,
-    add_icon_green,
-    add_icon_white,
-    bag_icon,
-    cross_icon,
-    tiktok_logo,
-    instagram_logo,
-    facebook_logo,
-    logout_icon,
-    parcel_icno,
-    rating_starts,
-    remove_icon_red,
-    selector_icon,
-    logo,
-    search_icon,
-    basket_icon,
-    header_room,
-    profile_icon
-}
+  basket_icon,
+  add_icon_green,
+  add_icon_white,
+  bag_icon,
+  cross_icon,
+  tiktok_logo,
+  instagram_logo,
+  facebook_logo,
+  logout_icon,
+  parcel_icno,
+  rating_starts,
+  remove_icon_red,
+  selector_icon,
+  logo,
+  search_icon,
+  basket_icon,
+  header_room,
+  profile_icon,
+};
 
 export const menu_list = [
-    {
-        menu_name:"სანთელი",
-        menu_image: candle_1
-    },
-    {
-        menu_name:"ქანდაკება",
-        menu_image: statue_2
-    },
-    {
-        menu_name:"ლიტერატურა",
-        menu_image: literature_1
-    },
-    {
-        menu_name:"ქვები და კრისტალები",
-        menu_image: crystal_1
-    },
-    {
-        menu_name:"ეთერზეთები",
-        menu_image: oil_1
-    },
-    {
-        menu_name:"მცენარეები",
-        menu_image: plant_1
-    },
-    {
-        menu_name:"თილისმები და ამულეტები",
-        menu_image: talisman_1
-    },
-    {
-        menu_name:"ტაროს დასტები და ორაკულები",
-        menu_image: tarot_1
-    },
-    {
-        menu_name:"ეზოთერიკული ნივთები",
-        menu_image: esoteric_item
-    },
-    {
-        menu_name:"მისტიკური აქსესუარები",
-        menu_image: accessories_1
-    },
-    {
-        menu_name:"ფენშუი ნივთები",
-        menu_image: fengshui_1
-    },
-    {
-        menu_name:"სურნელოვანი ჩხირები",
-        menu_image: sticks_1
-    }
-
-]
+  {
+    menu_name: "სანთელი",
+    menu_image: candle_1,
+  },
+  {
+    menu_name: "ქანდაკება",
+    menu_image: statue_2,
+  },
+  {
+    menu_name: "ლიტერატურა",
+    menu_image: literature_1,
+  },
+  {
+    menu_name: "ქვები და კრისტალები",
+    menu_image: crystal_1,
+  },
+  {
+    menu_name: "ეთერზეთები",
+    menu_image: oil_1,
+  },
+  {
+    menu_name: "მცენარეები",
+    menu_image: plant_1,
+  },
+  {
+    menu_name: "თილისმები და ამულეტები",
+    menu_image: talisman_1,
+  },
+  {
+    menu_name: "ტაროს დასტები და ორაკულები",
+    menu_image: tarot_1,
+  },
+  {
+    menu_name: "ეზოთერიკული ნივთები",
+    menu_image: esoteric_item,
+  },
+  {
+    menu_name: "მისტიკური აქსესუარები",
+    menu_image: accessories_1,
+  },
+  {
+    menu_name: "ფენშუი ნივთები",
+    menu_image: fengshui_1,
+  },
+  {
+    menu_name: "სურნელოვანი ჩხირები",
+    menu_image: sticks_1,
+  },
+];
 
 export const items_list = [
-    {
-        _id: "1",
-        name: "ნივთი1",
-        image: item_1,
-        price: 12,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "სანთელი"
-    },
-    {
-        _id: "2",
-        name: "ამულეტი",
-        image: item_2,
-        price: 17,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "3",
-        name: "ნივთი222",
-        image: item_3,
-        price: 41,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "4",
-        name: "ნივთი602",
-        image: item_4,
-        price: 33,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "მისტიკური აქსესუარები"
-    },
-    {
-        _id: "5",
-        name: "ნივთი19",
-        image: item_5,
-        price: 9,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "6",
-        name: "ნივთი101",
-        image: item_6,
-        price: 13,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "7",
-        name: "ნივთი 19",
-        image: item_7,
-        price: 18,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "ლიტერატურა"
-    },
-    {
-        _id: "8",
-        name: "ნივთი 4",
-        image: item_8,
-        price: 34,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "9",
-        name: "ნივთი9",
-        image: item_9,
-        price: 34,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "ლიტერატურა"
-    },
-    {
-        _id: "10",
-        name: "ნივთი908",
-        image: item_10,
-        price: 34,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "თილისმები და ამულეტები"
-    },
-    {
-        _id: "11",
-        name: "ნივთი678",
-        image: item_11,
-        price: 34,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "ქვები და კრისტალები"
-    },
-    {
-        _id: "12",
-        name: "ნივთი345",
-        image: item_12,
-        price: 34,
-        description: "ჯანმრთელობასა და კეთილდღეობას",
-        category: "სურნელოვანი ჩხირები"
-    },
-]
+  {
+    _id: "1",
+    name: "ნივთი1",
+    image: item_1,
+    price: 12,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "სანთელი",
+  },
+  {
+    _id: "2",
+    name: "ამულეტი",
+    image: item_2,
+    price: 17,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "3",
+    name: "ნივთი222",
+    image: item_3,
+    price: 41,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "4",
+    name: "ნივთი602",
+    image: item_4,
+    price: 33,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "მისტიკური აქსესუარები",
+  },
+  {
+    _id: "5",
+    name: "ნივთი19",
+    image: item_5,
+    price: 9,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "6",
+    name: "ნივთი101",
+    image: item_6,
+    price: 13,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "7",
+    name: "ნივთი 19",
+    image: item_7,
+    price: 18,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "ლიტერატურა",
+  },
+  {
+    _id: "8",
+    name: "ნივთი 4",
+    image: item_8,
+    price: 34,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "9",
+    name: "ნივთი9",
+    image: item_9,
+    price: 34,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "ლიტერატურა",
+  },
+  {
+    _id: "10",
+    name: "ნივთი908",
+    image: item_10,
+    price: 34,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "თილისმები და ამულეტები",
+  },
+  {
+    _id: "11",
+    name: "ნივთი678",
+    image: item_11,
+    price: 34,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "ქვები და კრისტალები",
+  },
+  {
+    _id: "12",
+    name: "ნივთი345",
+    image: item_12,
+    price: 34,
+    description: "ჯანმრთელობასა და კეთილდღეობას",
+    category: "სურნელოვანი ჩხირები",
+  },
+];
